6aca30a0759a2608509a3b17d574fedbfbd0fb29
JRH89 <thegemlins@gmail.com>
2024-08-28 23:46:42 -0700

    ok

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,11 +3,8 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>SaaSavant</span>,
-  project: {
-    link: "https://github.com/jrh89",
-  },
 
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://saasavant-docs.vercel.app",
   footer: {
     text: "SaaSavant",
   },

60d1dc5e3f4924c5e1842549ab68349ede12a7f7
JRH89 <thegemlins@gmail.com>
2024-09-01 08:49:47 -0700

    ok

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>SaaSavant</span>,
+  logo: <><Image style={{ borderRadius: '12%', border: '2px solid white' }} src="/logo.png" alt="SaaSavant Logo" width={40} height={40} />  <span style={{ marginLeft: 10, fontWeight: 'bold', fontSize: 20 }}>SaaSavant</span></>,
 
   docsRepositoryBase: "https://saasavant-docs.vercel.app",
   footer: {
